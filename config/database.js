2aa3e97089addf8a536b7a2bc665b9b1271004ee
Mark Dennis Napil <120807518+MarkDennisNapil@users.noreply.github.com>
2024-12-05 23:11:04 +0800

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,11 @@
+export const dbconfig = {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "mydb",
+} 
+export const tableConfig = {
+    tables: {
+        user: "users"
+    }
+}
